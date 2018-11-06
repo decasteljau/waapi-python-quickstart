411b8e90f6690c54a61c6b8bb40db8acd25372dd
Bernard Rodrigue <brodrigue@audiokinetic.com>
2018-11-06 16:01:15 -0500

    updated script to use the with statement

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
-from waapi import WaapiClient
+from waapi import WaapiClient, CannotConnectToWaapiException
+from pprint import pprint
 
-# Connect (default URL)
-client = WaapiClient()
+try:
+    # Connecting to Waapi using default URL
+    with WaapiClient() as client:
 
-# Simple RPC
-result = client.call("ak.wwise.core.getInfo")
-print(result)
+        # Simple RPC
+        result = client.call("ak.wwise.core.getInfo")
+        pprint(result)
 
-# RPC with options
-# return an array of all objects in the default actor-mixer work-unit
-args = {
-    "from": {"path": ['\\Actor-Mixer Hierarchy']},
-    "transform": [
-        {"select": ['children']}
-    ]
-}
-options = {
-    "return": ['id', 'name','type']
-}
-result = client.call("ak.wwise.core.object.get", args, options=options)
-print(result)
+        # RPC with options
+        # return an array of all children objects in the default actor-mixer work-unit
+        args = {
+            "from": {"path": ['\\Actor-Mixer Hierarchy\\Default Work Unit']},
+            "transform": [
+                {"select": ['children']}
+            ]
+        }
+        options = {
+            "return": ['id', 'name','type']
+        }
+        result = client.call("ak.wwise.core.object.get", args, options=options)
+        pprint(result)
 
-# Subscribe
-handler = client.subscribe(
-    "ak.wwise.core.object.created",
-    lambda object: print("Object created: " + str(object))
-)
+        # Subscribe
+        handler = client.subscribe(
+            "ak.wwise.core.object.created",
+            lambda object: print("Object created: " + str(object))
+        )
 
-# Bind a different callback at any time
-def my_callback(object):
-    print("Different callback: " + str(object))
+        # Bind a different callback at any time
+        def my_callback(object):
+            print("Different callback: " + str(object))
 
-handler.bind(my_callback)
+        handler.bind(my_callback)
 
-# Unsubscribe
-handler.unsubscribe()
+        # Unsubscribe
+        handler.unsubscribe()
 
-# Disconnect
-client.disconnect()
+except CannotConnectToWaapiException:
+    print("Could not connect to Waapi: Is Wwise running and Wwise Authoring API enabled?")
